72ecc7ab382841078841090de9b75befb56955ea
MichaelMayor <michael.davidge.mayor@gmail.com>
2020-11-13 16:23:12 -0500

    Deleted unneccesary files

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-// Import the sequelize object on which 
-// we have defined model. 
-const sequelize = require('./utils/database') 
+// Import the sequelize object on which
+// we have defined model.
+const sequelize = require("./utils/database");
 
-// Import the user model we have defined 
-const User = require('./models/user') 
+// Create all the table defined using
+// sequelize in Database
 
-// Create all the table defined using 
-// sequelize in Database 
+// Sync all models that are not
+// already in the database
+sequelize.sync();
 
-// Sync all models that are not 
-// already in the database 
-sequelize.sync() 
-
-// Force sync all models 
-// It will drop the table first 
-// and re-create it afterwards 
-sequelize.sync({force:true}) 
+// Force sync all models
+// It will drop the table first
+// and re-create it afterwards
+sequelize.sync({ force: true });
