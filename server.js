13522c9082051497cab6d78684854a3fdad9bae5
Rebecca88 <rebecca88oliver@gmail.com>
2020-11-13 21:02:54 -0500

    Deleted some files,  fixed sequelized hopefully

--- server.js
+++ server.js
@@ -1,28 +1,13 @@
-// Requiring necessary npm packages
-const express = require("express");
-const session = require("express-session");
-
-// Setting up port and requiring models for syncing
-const PORT = process.env.PORT || 8080;
-const db = require("./models");
-
-// Creating express app and configuring middleware needed for authentication
+const express = require ("express");
 const app = express();
-app.use(express.urlencoded({ extended: true }));
+const db = require('./models');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static("public"));
 
-// Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
-
-// Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
-});
+    app.listen(PORT, () => {
+        console.log(`listening at: http://localhost:${PORT}`);
+    });
+    });
