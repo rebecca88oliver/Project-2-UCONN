59ac08da034279c9f4d533e5babc958f7a411e8f
Elisha Badio <elisha.badio@uconn.edu>
2020-11-15 18:16:12 -0500

    update file for handlebars and css files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,29 +1,40 @@
-// Requiring path to so we can use relative routes to our HTML files
-const path = require("path");
-
-// Requiring our custom middleware for checking if a user is logged in
-const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render('dashboard')
   });
 
-  app.get("/login", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+  app.get("/createInventory", (req, res) => {
+    res.render('createInventory')
   });
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+  app.get("/addCategory", (req, res) => {
+    res.render('addCategory')
   });
+
+  app.get("/editCategory", (req, res) => {
+    res.render('editCategory')
+  });
+
+  app.get("/deleteCategory", (req, res) => {
+    res.render('deleteCategory')
+  });
+
+ 
+  app.get("/changeCategoryOrder", (req, res) => {
+    res.render('changeCategoryOrder')
+  });
+
+  app.get("/createProduct", (req, res) => {
+    res.render('createProduct')
+  });
+
+  app.get("/fullInventory", (req, res) => {
+    res.render('fullInventory')
+  });
+
+  app.get("/productList", (req, res) => {
+    res.render('productList')
+  });
+  
 };
