aad9f41f5b14a6b48626ad5a1cc6077d270802b0
MichaelMayor <michael.davidge.mayor@gmail.com>
2020-11-21 13:45:27 -0500

    revised env variable

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,12 +7,9 @@ const config = require("../config/config.json");
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    dialect: "mysql"
+  });
 }
 
 const queryInterface = sequelize.getQueryInterface();
