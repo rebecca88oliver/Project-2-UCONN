a60256720cc72185515c5a1c70cce2d7f009a76b
MichaelMayor <michael.davidge.mayor@gmail.com>
2020-11-21 13:38:28 -0500

    Added config require

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,6 +3,7 @@
 const db = require("../models");
 const { Sequelize, DataTypes } = require("sequelize");
 let sequelize;
+const config = require("../config/config.json");
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
